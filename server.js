2d0e4d965a37eb0df54aa2909fccc6279de23097
martuki <33644103+elmartuki@users.noreply.github.com>
2025-05-03 14:31:54 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -184,8 +184,17 @@ app.put('/inflacion/:id', (req, res) => {
 
 app.use(cors({
   origin: "https://inflacionsemanal.netlify.app",
-  credentials: true,
+  credentials: true
 }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", "https://inflacionsemanal.netlify.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
@@ -198,8 +207,13 @@ app.use(express.json());
 app.use(session({
   secret: 'clave-secreta',
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
+  cookie: {
+    sameSite: 'none',
+    secure: true
+  }
 }));
+
 
 // Servir archivos desde la misma carpeta
 app.use(express.static(__dirname));
