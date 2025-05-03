0ef9448ec265e59e973f5ecfb4ffdf4284c4a7ba
martuki <33644103+elmartuki@users.noreply.github.com>
2025-05-03 15:26:20 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -182,15 +182,18 @@ app.put('/inflacion/:id', (req, res) => {
 });
 
 
+const cors = require('cors');
+
 app.use(cors({
-  origin: "https://inflacionsemanal.netlify.app",
+  origin: 'https://inflacionsemanal.netlify.app',
   credentials: true
 }));
 
+// Esto es opcional pero ayuda para OPTIONS y compatibilidad total:
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Origin", "https://inflacionsemanal.netlify.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
